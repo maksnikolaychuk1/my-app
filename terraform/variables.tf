7b0aab8ab74c44a8b6144c4a6877a81d3a9f4d0d
maksnikolaychuk1 <maksym.nikolaichuk@oa.edu.ua>
2026-04-03 10:20:38 +0300

    Revert "terraform"
    This reverts commit 77b625272ac2eae1e6fe77607b587d744ef550ad.

--- terraform/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "vercel_api_token" {
-  description = "API-токен Vercel"
-  type        = string
-}
-variable "student_id" {
-  description = "Student ID for custom domain"
-  type        = string
-}
